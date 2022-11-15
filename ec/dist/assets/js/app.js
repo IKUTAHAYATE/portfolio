2bbf0c325af6eba7288d5b8efaa7032c22c92ed6
IKUTAHAYATE <39112536+IKUTAHAYATE@users.noreply.github.com>
2022-11-16 03:16:36 +0900

    各商品の出し分け処理

--- ec/dist/assets/js/app.js
+++ ec/dist/assets/js/app.js
@@ -3,7 +3,7 @@
 // ページローダー処理
 $(window).on('load', () => { $('.c-loader').fadeOut(); })
 
-$(() => {
+$(($) => {
     // slickをトップページのみ適用
     if( location.pathname === '/' ){
         // トップページスリック処理
@@ -34,86 +34,136 @@ $(() => {
     }
 
     // サイトで使用するアニメーション処理
-    class Animation {
-        constructor($trigger, $target) {
-            this.trigger =  $trigger;
-            this.target =  $target;
-            this.active = 'is-active';
-        }
-        execution() {
-            new Hamburger(
-                '[hamburger-menu="trigger"]',
-                '[hamburger-menu="target"]'
-            ).process()
-
-            new SizeBind(
-                '[size-bind="trigger"]',
-                '[size-bind="target"]'
-            ).process()
-
-            new Review(
-                '[review-item="trigger"]',
-                '[review-item="target"]'
-            ).process()
-
-            new ItemAccordion(
-                '[item-accordion="trigger"]',
-                '[item-accordion="targt"]'
-            ).process()
-        }
-    }
-    // ハンバーガーメニューclass化
-    class Hamburger extends Animation {
-        process() {
-            $(this.trigger).on('click', (e) => {
-                $(e.currentTarget).toggleClass(this.active);
-                $(this.target).toggleClass(this.active);
-            })
-        }
-    }
+    const runAnimation = () => { 
+        class Animation {
+            constructor($trigger, $target) {
+                this.trigger =  $trigger;
+                this.target =  $target;
+                this.active = 'is-active';
+            }
+            execution() {
+                new Hamburger(
+                    '[hamburger-menu="trigger"]',
+                    '[hamburger-menu="target"]'
+                ).process()
     
-    // 詳細ページSelectSize箇所テキストバインド処理
-    class SizeBind extends Animation {
-        process() {
-            $(this.trigger).on('click', (e) => {
-                const select_size = $(e.currentTarget).text();
-                $(e.currentTarget).addClass(this.active)
-                    .siblings('li').removeClass(this.active);
-                $(this.target).text(select_size);
-            })
+                new SizeBind(
+                    '[size-bind="trigger"]',
+                    '[size-bind="target"]'
+                ).process()
+    
+                new Review(
+                    '[review-item="trigger"]',
+                    '[review-item="target"]'
+                ).process()
+    
+                new ItemAccordion(
+                    '[item-accordion="trigger"]',
+                    '[item-accordion="targt"]'
+                ).process()
+            }
         }
+
+        // ハンバーガーメニューclass化
+        class Hamburger extends Animation {
+            process() {
+                $(this.trigger).on('click', (e) => {
+                    $(e.currentTarget).toggleClass(this.active);
+                    $(this.target).toggleClass(this.active);
+                })
+            }
+        }
+        
+        // 詳細ページSelectSize箇所テキストバインド処理
+        class SizeBind extends Animation {
+            process() {
+                $(this.trigger).on('click', (e) => {
+                    const select_size = $(e.currentTarget).text();
+                    $(e.currentTarget).addClass(this.active)
+                        .siblings('li').removeClass(this.active);
+                    $(this.target).text(select_size);
+                })
+            }
+        }
+    
+        // 詳細ペーレビュー機能実装
+        class Review extends Animation {
+            process() {
+                let review_num = 0;
+                $(this.trigger).on('click', (e) => {
+                    const _self = e.currentTarget;
+                    const _selfIndex = $(this.trigger).index(_self) + 1 ;
+                    if( review_num === _selfIndex ){
+                        $(this.trigger).removeClass(this.active);
+                        review_num = 0;
+                    }else{
+                        review_num = _selfIndex;
+                        $(this.trigger).removeClass(this.active);
+                        $(`.l-itemDetail__reviewItem:lt('${review_num}')`).addClass(this.active);
+                    }
+                    $(this.target).text(review_num);
+                })
+            }
+        }
+
+        // 商品説明アコーディオン
+        class ItemAccordion extends Animation {
+            process() {
+                $(this.trigger).on('click', (e) => {
+                    $(e.currentTarget).toggleClass(this.active);
+                    $(this.target).slideToggle();
+                })
+            }
+        }
+        const animation = new Animation();
+        animation.execution();
     }
 
-    // 詳細ペーレビュー機能実装
-    class Review extends Animation {
-        process() {
-            let review_num = 0;
-            $(this.trigger).on('click', (e) => {
-                const _self = e.currentTarget;
-                const _selfIndex = $(this.trigger).index(_self) + 1 ;
-                if( review_num === _selfIndex ){
-                    $(this.trigger).removeClass(this.active);
-                    review_num = 0;
-                }else{
-                    review_num = _selfIndex;
-                    $(this.trigger).removeClass(this.active);
-                    $(`.l-itemDetail__reviewItem:lt('${review_num}')`).addClass(this.active);
-                }
-                $(this.target).text(review_num);
-            })
+    // 各商品の出し分け処理
+    const addItems = () => {
+        class Item {
+            constructor() {
+                this.html_template = '';
+            }
+            createDom(items) {
+                items.forEach((item) => {
+                    this.html_template += `<li class="c-item" data-item-id="${item['id']}">
+                    <a href="detail.html?id=${item['id']}">
+                    <div class="c-item__cap">
+                    <img src="/assets/img/item/${item['id']}.png" loading="lazy">
+                    </div>
+                    <div class="c-item__info">
+                    <h3 class="c-item__name">${item['name']}</h3>
+                    <p class="c-item__text">${item['text']}</p>
+                    <div class="c-item__price">¥${item['price']}</div>
+                    </div>
+                    </a>
+                    </li>`;
+                })
+                return this.html_template;
+            }
+            execution() {
+                const itemList = new ItemList();
+                const news = itemList.getItemList('new');
+                $('[data-item-list="new"]').append(this.createDom(news))
+            }
         }
+        class ItemList {
+            getItemList(key) {
+                const items = item_data.filter((item) => {
+                    switch(key){
+                        case 'new':
+                            return item['new']
+                            break;
+                    }
+                })
+                return items;
+            }
+        }
+        const item = new Item();
+        item.execution();
     }
 
-    // 商品説明アコーディオン
-    class ItemAccordion extends Animation {
-        process() {
-            $(this.trigger).on('click', (e) => {
-                $(e.currentTarget).toggleClass(this.active);
-                $(this.target).slideToggle();
-            });
-        }
-    }
-
-    const animation = new Animation();
-    animation.execution();
-})
+    runAnimation();
+    addItems();
+})(jQuery)
