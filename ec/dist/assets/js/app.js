56bae475c1b0bcb44b1c480886dc5d7ccf6e45e9
IKUTAHAYATE <39112536+IKUTAHAYATE@users.noreply.github.com>
2022-11-15 17:29:30 +0900

    slickSliderトップページ実装

--- ec/dist/assets/js/app.js
+++ ec/dist/assets/js/app.js
@@ -1,1 +1,29 @@
-console.log(111);
+"use strict";
+
+$(() => {
+    const showSlick = () => {
+        $('#js-slider').slick({
+            arrows:true,
+            autoplay:true,
+            dots:true,
+            speed:1500,
+            easing:'swing',
+            centerMode:true,
+            centerPadding:'25%',
+            prevArrow:'<div class="l-slider__btn is-prev"></div>',
+            nextArrow:'<div class="l-slider__btn is-next"></div>',
+            responsive:[
+                {
+                    breakpoint:768,
+                    settings:{
+                        centerPadding:'0%',
+                        slidesToShow:1,
+                        slidesToScroll:1,
+                    }
+                }
+            ]
+        });
+    }
+
+    showSlick();
+});
