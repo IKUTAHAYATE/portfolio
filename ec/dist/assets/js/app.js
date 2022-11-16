fcb521c63e7d1fb8132c29ab7877b00e6cbf7ab1
IKUTAHAYATE <39112536+IKUTAHAYATE@users.noreply.github.com>
2022-11-16 19:32:15 +0900

    リストページもっと見るボタン押下時の挙動処理

--- ec/dist/assets/js/app.js
+++ ec/dist/assets/js/app.js
@@ -200,6 +200,46 @@ $(() => {
         item.execution();
     }
 
+    // リストページもっと見るボタン処理
+    const moreButton = () => {
+        // ブランドは3つずつ、商品は10つずつ表示させる
+        const more_count = {
+            'brand': 3,
+            'items': 10
+        }
+        class More {
+            constructor(brand, items) {
+                this.brand = brand;
+                this.items = items;
+            }
+            moreControl(el, num) {
+                const more_type = $(el).attr('data-more-btn'),
+                    target_list = $(`[data-more-list="${more_type}"]`),
+                    max_count = target_list.find('li').length;
+                more_count[more_type] += num;
+                target_list.find(`li:lt(${more_count[more_type]})`).fadeIn();
+                if( more_count[more_type] >= max_count ){
+                    $(el).hide();
+                }
+            }
+            execution() {
+                const _self = this;
+                $('[data-more-btn="brand"]').on('click',(e) => {
+                    _self.moreControl($(e.currentTarget), _self.brand);
+                })
+                $('[data-more-btn="items"]').on('click',(e) => {
+                    _self.moreControl($(e.currentTarget), _self.items);
+                })
+            }
+        }
+        const more = new More(
+            more_count['brand'],
+            more_count['items'],
+        )
+        more.execution();
+    }
+
     runAnimation();
     addItems();
+    moreButton();
 })
